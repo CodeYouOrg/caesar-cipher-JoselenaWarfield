8a3b67b4347ae6862ac2eceb794d89b62624738b
JoJo Warfield <jojowarfield@JoJos-MBP.lan>
2024-08-18 15:28:07 -0400

    Cipher.py update

--- cipher.py
+++ cipher.py
@@ -1,1 +1,36 @@
-
+sentence = input("Please entera sentence ")
+sentence = sentence.lower()
+alphabet_dict = {
+    "a": "f",
+    "b": "g",
+    "c": "h",
+    "d": "i",
+    "e": "j",
+    "f": "k",
+    "g": "l",
+    "h": "m",
+    "i": "n",
+    "j": "o",
+    "k": "p",
+    "l": "q",
+    "m": "r",
+    "n": "s",
+    "o": "t",
+    "p": "u",
+    "q": "v",
+    "r": "w",
+    "s": "x",
+    "t": "y",
+    "u": "z",
+    "v": "a",
+    "w": "b",
+    "x": "c",
+    "y": "d",
+    "z": "e",
+}
+cipher_sentence = ""
+for char in sentence:
+    if char in alphabet_dict:
+        char = alphabet_dict[char]
+    cipher_sentence += char
+print("The encrypted sentence is: " + cipher_sentence)
